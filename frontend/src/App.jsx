9d027a5b7124786bd41807d8dcfa80b91c534fd5
leehyeonseok-NightTraveler <fh0306@naver.com>
2025-11-24 17:17:00 +0900

    관리자 페이지 프론트앤드 리액트 변환 완료
    관리자 마이페이지 프론트쪽 리액트 변환 종료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NoticeList from './pages/Notice/notice_list.jsx'
+import AdminMypage from './pages/Mypage/AdminMypage.jsx'
 
 function App() {
     return (
         <Router>
             <div>
-                {/* 라우팅 */}
                 <Routes>
+                    {/* 공지사항 리스트 */}
                     <Route path="/notice_list" element={<NoticeList />} />
+
+                    {/* 관리자 마이페이지 */}
+                    <Route path="/admin/mypage" element={<AdminMypage />} />
                 </Routes>
             </div>
         </Router>
