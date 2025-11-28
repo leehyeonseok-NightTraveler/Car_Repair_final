5c36ca79640bea4629e01b414d35cc1724c657c9
jh0317 <kim031719@naver.com>
2025-11-28 10:25:30 +0900

    회원가입, 예약 충돌 수정
    회원가입, 예약 충돌 수정

--- src/main/java/com/boot/dao/StoreDAO.java
+++ src/main/java/com/boot/dao/StoreDAO.java
@@ -10,12 +10,6 @@ public interface StoreDAO {
     // 업체 등록 (성공 시 1 반환)
     int registerStore(StoreDTO dto);
     
-    /**
-     * 업체 회원가입 (tbl_store에 삽입)
-     * @param storeDTO (암호화된 DTO 객체)
-     */
-    public void registerStore(StoreDTO storeDTO);
-    
     List<StoreDTO> findAllStores();
     // 아이디 중복 체크 (필요시 사용)
     int checkId(String storeId);
